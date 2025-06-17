c54f817ed0026eb994ad1068cdc2b90902a75470
Ivan Becerra <becerraivan79@gmail.com>
2025-06-17 15:03:14 -0300

    SPRINT 03: fix bug de tabla

--- src/app/admin/components/tabla-usuarios/tabla-usuarios.component.ts
+++ src/app/admin/components/tabla-usuarios/tabla-usuarios.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MaterialModule } from '../../../material.module';
 import { UsuarioService } from '../../../core/services/usuario.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tabla-usuarios',
   imports: [MaterialModule,
     CommonModule,
     FormsModule,
-    ReactiveFormsModule],
+    ReactiveFormsModule, NgIf, NgFor],
   templateUrl: './tabla-usuarios.component.html',
   styleUrl: './tabla-usuarios.component.scss'
 })
-export class TablaUsuariosComponent  implements OnInit{
+export class TablaUsuariosComponent  implements OnInit, OnDestroy{
   usuarios: any[] = [];
   roles: string[] = ['paciente', 'especialista'];
   rolSeleccionado: string = 'especialista';
@@ -21,27 +22,36 @@ export class TablaUsuariosComponent  implements OnInit{
   usuariosFiltrados: any[] = [];
   columnas: string[] = ['foto', 'nombre', 'dni', 'edad', 'email', 'rol', 'estado', 'acciones'];
   
+  private suscripcion!: Subscription;
   constructor(private usuarioService: UsuarioService) {
-    this.listar();
   }
+
   ngOnInit(): void { 
     this.cargarUsuarios();
   }
   cargarUsuarios() {
-    this.usuarioService.getUsuarios().subscribe(data => {
+   this.suscripcion = this.usuarioService.getUsuarios().subscribe(data => {
       this.usuarios = data;
+      console.log('subcribe');
+      console.log(this.usuarios);
+      console.log('...................');
+
       this.filtrarUsuarios('especialista');
     });
   }
-
-  
+  ngOnDestroy() {
+    console.log("se salio");
+     if (this.suscripcion) {
+      this.suscripcion.unsubscribe();
+    }
+  }
   filtrarUsuarios(rol: string) {
     this.rolSeleccionado = rol;
-
     switch (rol) {
       case 'paciente':
         this.columnas = ['foto', 'nombre', 'dni', 'edad', 'email', 'rol'];
         this.usuariosFiltrados = this.usuarios.filter(u => u.rol === 'paciente');
+
         break;
       case 'especialista':
         this.columnas = ['foto', 'nombre', 'dni', 'edad', 'email', 'rol', 'estado', 'acciones'];
