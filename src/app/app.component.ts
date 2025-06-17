93f8c6339b268a27558ce7ac7c204c1a2da0ef9f
Ivan Becerra <becerraivan79@gmail.com>
2025-06-16 21:26:59 -0300

    SPRINT 03: MODALES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TestImagenComponent } from "./test/test-imagen/test-imagen.component";
 import { MaterialModule } from './material.module';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MaterialModule, NavbarComponent],
+  imports: [RouterOutlet, MaterialModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
