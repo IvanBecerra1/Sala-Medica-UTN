aead62b3b93fcdaa13a9568124fafa56ee363636
Ivan Becerra <becerraivan79@gmail.com>
2025-06-23 20:08:43 -0300

    SPRINT 04: Graficos

--- src/app/core/services/estadisticas.service.ts
+++ src/app/core/services/estadisticas.service.ts
@@ -7,6 +7,32 @@ import { collection, Firestore, getDocs } from '@angular/fire/firestore';
 export class EstadisticasService {
  constructor(private firestore: Firestore) {}
 
+ 
+  async obtenerLogDeIngresos() {
+    const ref = collection(this.firestore, 'sala_medica_logs');
+    const snapshot = await getDocs(ref);
+
+    const logsPorDiaYUsuario: { [dia: string]: { [usuario: string]: number } } = {};
+
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      const timestamp = data['fecha'];
+      const nombre = data['nombre'];
+      const apellido = data['apellido'];
+
+      if (timestamp && nombre && apellido) {
+        const fecha = timestamp.toDate(); // timestamp → Date
+        const dia = fecha.toISOString().split('T')[0]; // YYYY-MM-DD
+        const usuario = `${nombre} ${apellido}`;
+
+        logsPorDiaYUsuario[dia] = logsPorDiaYUsuario[dia] || {};
+        logsPorDiaYUsuario[dia][usuario] = (logsPorDiaYUsuario[dia][usuario] || 0) + 1;
+      }
+    });
+
+    return logsPorDiaYUsuario;
+  }
+
   async obtenerTurnosPorDia() {
     const ref = collection(this.firestore, 'sala_medica_turno');
     const snapshot = await getDocs(ref);
@@ -34,10 +60,15 @@ export class EstadisticasService {
     const resultado: { [medico: string]: number } = {};
 
     snapshot.forEach(doc => {
-      const data : any = doc.data();
+    /*  const data : any = doc.data();
       const fecha = data.fechaSolicitado?.toDate?.();
+      if (!fecha) return;*/
+      
+      const data: any = doc.data();
+      const fechaStr = data.fechaTurno;
+      const fecha = fechaStr ? new Date(fechaStr) : null;
+      
       if (!fecha) return;
-
       if (data.estado === 'realizado' && fecha >= desde && fecha <= hasta) {
         const medico = `${data.especialistaNombre} ${data.especialistaApellido}`;
         resultado[medico] = (resultado[medico] || 0) + 1;
@@ -53,8 +84,10 @@ export class EstadisticasService {
     const resultado: { [medico: string]: number } = {};
 
     snapshot.forEach(doc => {
-      const data : any = doc.data();
-      const fecha = data.fechaSolicitado?.toDate?.();
+      const data: any = doc.data();
+      const fechaStr = data.fechaTurno;
+      const fecha = fechaStr ? new Date(fechaStr) : null;
+
       if (!fecha) return;
 
       if (fecha >= desde && fecha <= hasta) {
--- src/app/shared/components/graficos/graficos.component.ts
+++ src/app/shared/components/graficos/graficos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChartOptions, ChartType, ChartData } from 'chart.js';
 import { LogsService } from '../../../core/services/logs.service';
 import { NgChartsModule } from 'ng2-charts';
@@ -6,6 +6,7 @@ import { NgChartsModule } from 'ng2-charts';
 import { CommonModule } from '@angular/common';
 import { EstadisticasService } from '../../../core/services/estadisticas.service';
 import { MaterialModule } from '../../../material.module';
+import { jsPDF } from 'jspdf';
 @Component({
   selector: 'app-graficos',  
   imports: [CommonModule, NgChartsModule, MaterialModule],
@@ -13,9 +14,14 @@ import { MaterialModule } from '../../../material.module';
   styleUrl: './graficos.component.scss'
 })
 export class GraficosComponent implements OnInit {
+  @ViewChild('graficoDia') graficoDia!: ElementRef;
+  @ViewChild('graficoEspecialidad') graficoEspecialidad!: ElementRef;
+  @ViewChild('graficoSolicitados') graficoSolicitados!: ElementRef;
+  @ViewChild('graficoFinalizados') graficoFinalizados!: ElementRef;
+  @ViewChild('graficoLogs') graficoLogs!: ElementRef;
+
   graficoSeleccionado = 'dia';
 
-  // Opciones de gráficos
   barChartOptions: ChartOptions<'bar'> = {
     responsive: true,
     plugins: {
@@ -32,7 +38,7 @@ export class GraficosComponent implements OnInit {
     }
   };
 
-  // Datos de cada gráfico
+  logIngresosChartData!: ChartData<'bar'>;
   turnosPorDiaChartData: ChartData<'bar'> = { labels: [], datasets: [{ data: [], label: 'Turnos por Día' }] };
   turnosPorEspecialidadChartData: ChartData<'pie'> = { labels: [], datasets: [{ data: [] }] };
   turnosSolicitadosChartData: ChartData<'bar'> = { labels: [], datasets: [{ data: [], label: 'Solicitados por Médico' }] };
@@ -40,13 +46,51 @@ export class GraficosComponent implements OnInit {
 
   constructor(private estadisticasService: EstadisticasService) {}
 
+
+  logChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Ingresos al sistema por usuario y día'
+      },
+      legend: {
+        position: 'top',
+      }
+    }
+  };
+
   async ngOnInit() {
     await this.cargarTurnosPorDia();
     await this.cargarTurnosPorEspecialidad();
     await this.cargarTurnosSolicitados();
     await this.cargarTurnosFinalizados();
+    await this.cargarLogsUsuario();
+
   }
 
+  async cargarLogsUsuario(){
+    const datos = await this.estadisticasService.obtenerLogDeIngresos();
+
+    const dias = Object.keys(datos).sort();
+    const usuariosSet = new Set<string>();
+
+    dias.forEach(dia => {
+      Object.keys(datos[dia]).forEach(usuario => usuariosSet.add(usuario));
+    });
+
+    const usuarios = Array.from(usuariosSet);
+    const datasets = usuarios.map(usuario => ({
+      label: usuario,
+      data: dias.map(dia => datos[dia][usuario] || 0)
+    }));
+
+    this.logIngresosChartData = {
+      labels: dias,
+      datasets: datasets
+    };
+  
+  }
   async cargarTurnosPorDia() {
     const datos = await this.estadisticasService.obtenerTurnosPorDia();
     console.log('TURNOS POR DIA');
@@ -69,9 +113,8 @@ export class GraficosComponent implements OnInit {
   }
 
   async cargarTurnosSolicitados() {
-    const desde = new Date();
-    desde.setMonth(desde.getMonth() - 1); // último mes
-    const hasta = new Date();
+    const desde = new Date('2025-06-23'); 
+    const hasta = new Date('2025-07-23'); 
 
     const datos = await this.estadisticasService.obtenerTurnosSolicitadosPorMedico(desde, hasta);
     console.log('TURNOS SOLICITADO');
@@ -83,9 +126,9 @@ export class GraficosComponent implements OnInit {
   }
 
   async cargarTurnosFinalizados() {
-    const desde = new Date();
-    desde.setMonth(desde.getMonth() - 1);
-    const hasta = new Date();
+    const desde = new Date('2025-06-23'); 
+    const hasta = new Date('2025-07-23'); 
+
 
     const datos = await this.estadisticasService.obtenerTurnosFinalizadosPorMedico(desde, hasta);
     console.log('TURNOS FINALIZADOS');
@@ -94,5 +137,37 @@ export class GraficosComponent implements OnInit {
       labels: datos.map(d => d.medico),
       datasets: [{ data: datos.map(d => d.cantidad), label: 'Finalizados por Médico' }]
     };
+    }
+  exportarGraficoActual() {
+    switch (this.graficoSeleccionado) {
+      case 'dia':
+        this.generarPdfDelGrafico(this.graficoDia, 'Turnos por Día', 'turnos_dia.pdf');
+        break;
+      case 'especialidad':
+        this.generarPdfDelGrafico(this.graficoEspecialidad, 'Turnos por Especialidad', 'turnos_especialidad.pdf');
+        break;
+      case 'solicitados':
+        this.generarPdfDelGrafico(this.graficoSolicitados, 'Turnos Solicitados por Médico', 'turnos_solicitados.pdf');
+        break;
+      case 'finalizados':
+        this.generarPdfDelGrafico(this.graficoFinalizados, 'Turnos Finalizados por Médico', 'turnos_finalizados.pdf');
+        break;
+      case 'logs':
+        this.generarPdfDelGrafico(this.graficoLogs, 'Logs de Ingreso al Sistema', 'logs_ingresos.pdf');
+        break;
+    }
   }
+
+  generarPdfDelGrafico(idCanvas: ElementRef, titulo: string, nombreArchivo: string) {
+    const canvas: HTMLCanvasElement = idCanvas.nativeElement;
+    const imgData = canvas.toDataURL('image/png');
+
+    const doc = new jsPDF('landscape');
+    doc.setFontSize(16);
+    doc.text(titulo, 15, 20);
+    doc.addImage(imgData, 'PNG', 10, 30, 270, 130); // ajusta tamaño
+
+    doc.save(nombreArchivo);
+  }
+
 }
